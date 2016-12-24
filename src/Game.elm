5f48abde212f0f984f8ab13283de0cb4e4ad2625
Théo Zimmermann <theo.zimmermann@univ-paris-diderot.fr>
2016-12-24 18:34:46 +0100

    Make the game interactive.

--- src/Game.elm
+++ src/Game.elm
@@ -1,16 +1,19 @@
 module Game exposing (..)
 
 import Html exposing (Html)
+import Keyboard exposing (KeyCode)
 import Lib exposing (..)
 
 
-main : Program Never State msg
+main : Program Never State Msg
 main =
     Html.program
         { init = pureState initState
         , view = view
         , update = pureUpdate update
-        , subscriptions = \_ -> Sub.none
+        , subscriptions =
+            \_ -> Keyboard.presses Key
+            -- TODO: stop listening to keyboard during one second after change of state
         }
 
 
@@ -33,8 +36,12 @@ toInt nat =
             1 + toInt nat
 
 
+type alias Answer =
+    Maybe Int
+
+
 type QuestionState
-    = RemainingTrials Nat
+    = RemainingTrials ( Nat, Answer )
     | Done Int
 
 
@@ -46,18 +53,23 @@ type alias State =
     }
 
 
-maxTrials : Nat
-maxTrials =
-    Succ (Succ Zero)
-
-
 initState : State
 initState =
     { remaining = [ ( 2, 3, 6 ), ( 6, 7, 42 ) ]
     , done = []
     , currentQuestion = ( 9, 3, 27 )
-    , questionState = RemainingTrials maxTrials
+    , questionState = initQuestionState
     }
+
+
+initQuestionState : QuestionState
+initQuestionState =
+    RemainingTrials ( initRemainings, Nothing )
+
+
+initRemainings : Nat
+initRemainings =
+    Succ Zero
 
 
 view : State -> Html msg
@@ -89,11 +101,15 @@ viewDoneQuestion ( ( x, y, res ), score ) =
 
 
 viewCurrent : ( a, b, c ) -> QuestionState -> Html msg
-viewCurrent ( x, y, _ ) state =
-    Html.div []
-        [ Html.div [] [ viewQuestion x y ++ "?" |> Html.text ]
-        , Html.div [] [ viewQuestionState state |> Html.text ]
-        ]
+viewCurrent ( x, y, res ) state =
+    let
+        ( info, answer ) =
+            viewQuestionState (toString res) state
+    in
+        Html.div []
+            [ Html.div [] [ viewQuestion x y ++ answer |> Html.text ]
+            , Html.div [] [ info |> Html.text ]
+            ]
 
 
 viewQuestion : a -> b -> String
@@ -104,16 +120,107 @@ viewQuestion x y =
         ++ " = "
 
 
-viewQuestionState : QuestionState -> String
-viewQuestionState state =
+viewQuestionState : String -> QuestionState -> ( String, String )
+viewQuestionState default state =
     case state of
-        RemainingTrials nat ->
-            (nat |> toInt |> toString) ++ " essais restants."
+        RemainingTrials ( nat, answer ) ->
+            ( (nat |> toInt |> (+) 1 |> toString) ++ " essais restants."
+            , case answer of
+                Nothing ->
+                    "?"
+
+                Just x ->
+                    toString x
+            )
 
         Done points ->
-            toString points ++ "points."
+            ( toString points ++ " points.", default )
 
 
-update : msg -> State -> State
+type Msg
+    = Key KeyCode
+
+
+update : Msg -> State -> State
 update msg state =
-    state
+    case msg of
+        Key k ->
+            let
+                key =
+                    keyInterp k
+            in
+                case
+                    ( state.questionState
+                    , state.currentQuestion
+                    , state.remaining
+                    , key
+                    )
+                of
+                    ( RemainingTrials r, ( _, _, res ), _, _ ) ->
+                        { state | questionState = updateQuestionState res key r }
+
+                    ( Done points, question, newQuestion :: questions, Enter ) ->
+                        { remaining = questions
+                        , done = ( question, points ) :: state.done
+                        , currentQuestion = newQuestion
+                        , questionState = initQuestionState
+                        }
+
+                    _ ->
+                        state
+
+
+type KeyInterp
+    = Digit Int
+    | Enter
+    | Backspace
+    | Ignore
+
+
+keyInterp : KeyCode -> KeyInterp
+keyInterp key =
+    if key >= 48 && key <= 57 then
+        Digit (key - 48)
+    else if key == 13 then
+        Enter
+    else if key == 8 then
+        Backspace
+    else
+        Ignore
+
+
+updateQuestionState : Int -> KeyInterp -> ( Nat, Answer ) -> QuestionState
+updateQuestionState res key ( nat, answer ) =
+    case ( key, nat, answer ) of
+        ( Digit d, nat, Nothing ) ->
+            RemainingTrials ( nat, Just d )
+
+        ( Digit d, nat, Just ans ) ->
+            RemainingTrials ( nat, Just <| ans * 10 + d )
+
+        ( Backspace, nat, Just ans ) ->
+            RemainingTrials ( nat, Just <| ans // 10 )
+
+        ( Enter, nat, Just ans ) ->
+            if ans == res then
+                Done (computePoints nat)
+            else
+                case nat of
+                    Zero ->
+                        Done 0
+
+                    Succ nat ->
+                        RemainingTrials ( nat, Nothing )
+
+        _ ->
+            RemainingTrials ( nat, answer )
+
+
+computePoints : Nat -> number
+computePoints nat =
+    case nat of
+        Zero ->
+            1
+
+        Succ nat ->
+            2 * (computePoints nat) + 1
